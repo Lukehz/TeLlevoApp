3a78974698c6c77926f92e72303f6b45f5fff05e
Lukehz <Luc.hernandez@duocuc.cl>
2022-10-24 02:26:39 -0300

    SeAgrego Map con page

---
+++ src/app/mapa/mapa.page.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+//import { title } from 'process';
+
+declare var google;
+
+interface Marker {
+  position: {
+    lat: number,
+    lng: number,
+  };
+  title: string;
+}
+
+@Component({
+  selector: 'app-mapa',
+  templateUrl: './mapa.page.html',
+  styleUrls: ['./mapa.page.scss'],
+})
+export class MapaPage implements OnInit {
+
+  map: null;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    // create a new map by passing HTMLElement
+    const mapEle: HTMLElement = document.getElementById('map');
+    // create LatLng object
+    const myLatLng = {lat: -41.47015899249667, lng: -72.92578711596433};
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 16
+    });
+  
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      mapEle.classList.add('show-map');
+      const marker = {
+        position: {
+          lat: -41.47015899249667,
+          lng: -72.92578711596433
+        },
+        title: 'DUOC UC'
+      };
+      this.addMarker(marker);
+    });
+  }
+
+  addMarker(marker: Marker) {
+    return new google.maps.Marker({
+      position: marker.position,
+      map: this.map,
+      title: marker.title
+    });
+  }
+}
